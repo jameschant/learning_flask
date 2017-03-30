8a5f1ba7341c1af0352c90b8190feb7c4040a181
James Chant <jamesrchant@gmail.com>
2017-03-30 21:18:17 +0100

    Added contact form validation and bootstrap

--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 
 class SignupForm(Form):
-    first_name = StringField('First name')
-    last_name = StringField('Last name')
-    email = StringField('Email')
-    password = PasswordField('Password')
+    first_name = StringField('First name', validators=[DataRequired("First name required.")])
+    last_name = StringField('Last name', validators=[DataRequired("Last name required.")])
+    email = StringField('Email', validators=[DataRequired("Email required."), Email("Please enter a valid email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Password required."), Length(min=6, message="Password must be more than 6 characters long.")])
     submit = SubmitField('Sign up')
